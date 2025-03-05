caa7c1c3a43658bd508f9eca7971f95b9a3be0b7
Rugved <guravrugved@gmail.com>
2025-03-05 12:05:18 +0530

    step3 complete

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const add = require("./stringCalculator");
 
-console.log(add("")); // Expected output: 0
-console.log(add("1,2,3")); // Expected output: 6
-console.log(add("10,20,30")); // Expected output: 60
+console.log(add(""));
+console.log(add("1,2,3"));
+console.log(add("10,20,30"));
+console.log(add("1\n2,3"));
+console.log(add("1\n2,3"));
--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,7 @@
 function add(numbers) {
   if (!numbers) return 0; // Return 0 for an empty string
 
-  const numArray = numbers.split(",").map(Number); // Convert string to array of numbers
+  const numArray = numbers.replace(/\n/g, ",").split(",").map(Number);
 
   return numArray.reduce((sum, num) => sum + num, 0); // Sum the numbers
 }
